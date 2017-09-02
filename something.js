070b1c2c9e36de4ae920e901d1df7fa5fb8d7947
Gabriel3014 <30421216+Gabriel3014@users.noreply.github.com>
2017-09-02 19:15:57 -0300

    Update something.js

--- something.js
+++ something.js
@@ -16,3 +16,29 @@ function encode(str,n){
     return encode(str,n-1)
 }
 }
+decodebase64 = function(){
+	var text = $('#inputField').val();
+    if (!text.trim()) return console.warn("No save to import provided.");
+    if(text.length % 4 !== 0) {
+        console.log("String is not valid base64 encoded: " + text.length + ' (' + text.length % 4 + ')');
+        return;
+    }
+
+    var decompressed = LZString.decompressFromBase64(text);
+    if(!decompressed) {
+        console.log("Import Game failed, could not decompress!");
+        return;
+    }
+    $('#outputField').val(decompressed);
+}
+
+encodebase64 = function(){
+	var data = $('#inputField').val();
+
+    var string = JSON.stringify(data);
+    var compressed = LZString.compressToBase64(string);
+
+    console.log('Compressing Save');
+    console.log('Compressed from ' + string.length + ' to ' + compressed.length + ' characters');
+    $('#outputField').val(compressed);
+}
